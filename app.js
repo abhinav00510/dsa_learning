b5beaf752f9eaea5f1875428d5bbcdac32f66695
Abhinav Shrivastva <abhinav10shrivastav@gmail.com>
2026-01-07 23:14:15 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// let prompt = require('prompt-sync')()
+
+// let age = Number(prompt("Enter your age : "))
+// console.log(typeof(age));
+
+
+
+
+// swapping without using temp variable
+// let a = 20;
+// let b = 10;
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// console.log(`a = ${a}, b = ${b}`);
+
+
+
